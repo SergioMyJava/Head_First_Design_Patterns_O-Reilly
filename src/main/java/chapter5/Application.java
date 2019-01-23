459245c19572e31ca8e9a72002d43d43b6aaecae
Sergio1234567z <raboch2014@mail.ru>
2019-01-23 14:49:29 +0300

    Com

--- src/main/java/chapter5/Application.java
+++ src/main/java/chapter5/Application.java
@@ -6,4 +6,45 @@ public class Application {
 
 
     }
+
+    public static class SingltonOne{
+        private static SingltonOne singltonOne;
+
+        private SingltonOne(){}
+
+        public static SingltonOne getSingltonOne() {
+            if(singltonOne == null){
+                singltonOne = new SingltonOne();
+            }
+            return singltonOne;
+        }
+    }
+
+    public static class SingltonTwo{
+        private static volatile SingltonTwo singltonTwo;
+
+        private SingltonTwo(){}
+
+        public static SingltonTwo getSingltonTwo(){
+            if(singltonTwo == null){
+                synchronized (SingltonTwo.class){
+                    singltonTwo = new SingltonTwo();
+                }
+            }
+        return singltonTwo;
+        }
+    }
+
+    public static class SingltonThree{
+        private static SingltonThree singltonThree;
+
+        private SingltonThree(){}
+
+        public synchronized static SingltonThree getSingltonThree(){
+            if(singltonThree == null){
+                singltonThree = new SingltonThree();
+                }
+            return singltonThree;
+        }
+    }
 }
