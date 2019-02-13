f7c5e1992725a64579c4baee089d98548bb6cf53
Sergio1234567z <raboch2014@mail.ru>
2019-02-13 22:51:03 +0300

    Does not transmit the picture

--- src/main/java/chapter11/imageproxy/ImageProxyTestDrive.java
+++ src/main/java/chapter11/imageproxy/ImageProxyTestDrive.java
@@ -13,35 +13,41 @@ public class ImageProxyTestDrive {
     JMenu menu;
     Hashtable<String, String> cds = new Hashtable<String, String>();
     public static void main (String[] args) throws Exception {
-        ImageProxyTestDrive testDrive = new ImageProxyTestDrive();                      //тут как я понимаю мы создаем класс в котором создается
-    }                                                                                   //меню кнопки ну и прикрепляются ссылки к ним
+        ImageProxyTestDrive testDrive = new ImageProxyTestDrive();                      //тут как я понимаю мы создаем
+                                                                                        // класс в котором создается
+    }                                                                      //меню кнопки ну и прикрепляются ссылки к ним
     public ImageProxyTestDrive() throws Exception{
-        cds.put("Buddha Bar","http://images.amazon.com/images/P/B00009XBYK.01.LZZZZZZZ.jpg");// собственно кнопки с адресами
-        cds.put("Ima","http://images.amazon.com/images/P/B000005IRM.01.LZZZZZZZ.jpg");
+        cds.put("Buddha Bar","http://images.amazon.com/images/P/B00009XBYK.01.LZZZZZZZ.jpg");// собственно кнопки с
+        cds.put("Ima","http://images.amazon.com/images/P/B000005IRM.01.LZZZZZZZ.jpg");       //адресами
         cds.put("Karma","http://images.amazon.com/images/P/B000005DCB.01.LZZZZZZZ.gif");
         cds.put("MCMXC A.D.","http://images.amazon.com/images/P/B000002URV.01.LZZZZZZZ.jpg");
         cds.put("Northern Exposure","http://images.amazon.com/images/P/B000003SFN.01.LZZZZZZZ.jpg");
         cds.put("Selected Ambient Works, Vol. 2","http://images.amazon.com/images/P/B000002MNZ.01.LZZZZZZZ.jpg");
-        URL initialURL = new URL((String)cds.get("Selected Ambient Works, Vol. 2")); // тут какимто чудным образом происходит инициализация картинки
-                                                                                    // я так понимаю это по умолчанию стоит "Selected Ambient Works, Vol. 2" или что?
+        URL initialURL = new URL((String)cds.get("Selected Ambient Works, Vol. 2")); // тут какимто чудным образом
+                                                                                  // происходит инициализация картинки
+                                                                                    // я так понимаю это по умолчанию
+                                                                      //стоит "Selected Ambient Works, Vol. 2" или что?
         menuBar = new JMenuBar();                                                   //создаем меню
         menu = new JMenu("Favorite CDs");                                        //создаем надпись меню
         menuBar.add(menu);                                                          //добавляем надпись в название меню
         frame.setJMenuBar(menuBar);                                                 //добавляем созданное меню в рамку
-        for(Enumeration e = cds.keys(); e.hasMoreElements();) {                     //тут хрен его знает что происходит, но как я предпологаю по ключу(кнопке)
-            String name = (String)e.nextElement();                                  //нажатой по URL получается картинка и передается в наши классы
+        for(Enumeration e = cds.keys(); e.hasMoreElements();) {                     //тут хрен его знает что происходит,
+                                                                                // но как я предпологаю по ключу(кнопке)
+            String name = (String)e.nextElement();                                  //нажатой по URL получается картинка
+                                                                                   // и передается в наши классы
             JMenuItem menuItem = new JMenuItem(name);
             menu.add(menuItem);
             menuItem.addActionListener(event -> {
-                imageComponent.setIcon(new ImageProxy(getCDUrl(event.getActionCommand()))); // тут посредствам лямбда выражения это происходит, но что тут происходит у меня не получается
-                frame.repaint();                                                            //дебажил и getCDUrl возвращает null
+                imageComponent.setIcon(new ImageProxy(getCDUrl(event.getActionCommand()))); // тут посредствам лямбда
+                                                // выражения это происходит, но что тут происходит у меня не получается
+                frame.repaint();                                                    //дебажил и getCDUrl возвращает null
             });
         }
 // set up frame and menus
         Icon icon = new ImageProxy(initialURL);
         imageComponent = new ImageComponent(icon);
-        frame.getContentPane().add(imageComponent);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(imageComponent);          //тоже не понятные методы
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//тоже не понятные методы
         frame.setSize(800,600);
         frame.setVisible(true);
 
